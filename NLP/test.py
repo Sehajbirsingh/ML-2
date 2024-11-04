7612bce41e1e35c496825e14193b702345208e33
SehajbirSingh <sehajbakshi12@gmail.com>
2024-11-03 21:41:44 -0500

    update

---
+++ NLP/test.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+myurl = "https://stackoverflow.com/questions/415511/how-to-get-the-current-time-in-python"
+html = 
+soupified = 
+question_text = 
+print(f"Question = \n {question_text.get_text().strip()}")
+print("\n\n\n")
+
+answer_text = 
+answer = 
+print(f"Answer = \n {answer.get_text().strip()}")
